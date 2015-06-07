fab07bd1536e21055b7bff50fcffdab66fa24431
Douglas Perkins <webmaster@dperkins.org>
2015-06-08 00:37:45 +0900

    Adding more word source information.

--- frequency/get_list.sh
+++ frequency/get_list.sh
@@ -3,6 +3,9 @@
 # Downloads the list of words and frequencies.
 # This list was created in 2008 by Michiel Kamermans.
 # It has not been updated since.
+#
+# ftp://ftp.edrdg.org/pub/Nihongo/00INDEX.html
+# http://pomax.nihongoresources.com/index.php?entry=1222520260
 
 wget ftp://ftp.edrdg.org/pub/Nihongo/term_aggregates.zip
 unzip term_aggregates.zip
