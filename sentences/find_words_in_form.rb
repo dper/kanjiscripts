22188683c8057b57b62589e7064104aadb5ec14f
Douglas Perkins <webmaster@dperkins.org>
2015-11-15 23:23:32 +0900

    Loading form data appropriately.

--- sentences/find_words_in_form.rb
+++ sentences/find_words_in_form.rb
@@ -14,28 +14,27 @@
 # == AUTHOR
 # Douglas Perkins - https://dperkins.org - https://microca.st/dper
 
-require "cgi"
-cgi = CGI.new
-puts cgi.header
-puts "<html><body>This is a test</body></html>"
-
 Script_dir = File.dirname(__FILE__)
+require "cgi"
 require './word_finder.rb'
 
 # Finds words in sentences and makes lists of those sentences.
 class Text_Finder
-	TARGET_SENTENCE_COUNT = 3
-	MAX_SENTENCE_LENGTH = 25
+	# Stores information from forms.
+	def get_parameters cgi
+		@max_sentence_length = cgi['max_sentence_length']
+		@sentences_per_word = cgi['sentences_per_word']
+
+		text = cgi['words']
+
+		words = text.split
+		@words = words.uniq
+	end
 
 	# Creates a Finder.
-	def initialize
-		#TODO Get the text.
-
-		words = []
-		text.each { |line| words.concat line.split }
-		@words = words.uniq
-
-		@finder = Finder.new MAX_SENTENCE_LENGTH
+	def initialize cgi
+		get_parameters cgi
+		@finder = Finder.new @max_sentence_length
 		corpus_information = @finder.get_corpus_information
 		unique = corpus_information['unique_pairs']
 		short = corpus_information['short_pairs']
@@ -45,15 +44,15 @@ class Text_Finder
 
 	# Looks up the words in the corpus.
 	def find_sentences
-		puts 'Maximum sentence length: ' + MAX_SENTENCE_LENGTH.to_s + '.'
-		puts 'Desired sentences per word: ' + TARGET_SENTENCE_COUNT.to_s + '.'
+		puts 'Maximum sentence length: ' + @max_sentence_length.to_s + '.'
+		puts 'Desired sentences per word: ' + @sentences_per_word.to_s + '.'
 		puts 'Words sought: ' + @words.length.to_s + '.'
-		sought = TARGET_SENTENCE_COUNT * @words.length
+		sought = @sentences_per_word * @words.length
 		puts 'Sentences sought: ' + sought.to_s + '.'
 		puts 'Finding words in the corpus ...'
 		puts '-------------------------------'
 		
-		totals = @finder.find_sentences @words, TARGET_SENTENCE_COUNT
+		totals = @finder.find_sentences @words, @sentences_per_word
 		
 		@finder.get_search_information.each do |result|
 			puts result['found'].to_s + "\t" + result['word']
@@ -78,6 +77,20 @@ class Text_Finder
 	end
 end
 
-#$text_finder = Text_Finder.new
+cgi = CGI.new
+puts cgi.header
+puts "<!DOCTYPE html>\n"
+puts "<html>\n"
+puts "<head>\n"
+puts "<meta charset=\"utf-8\" />\n"
+puts "</head>\n"
+puts "<body>\n"
+puts "Yay."
+
+$text_finder = Text_Finder.new cgi
 #$text_finder.find_sentences
 #$text_finder.write_sentences
+
+puts "Yay again."
+puts "</body>\n"
+puts "</html>\n"
